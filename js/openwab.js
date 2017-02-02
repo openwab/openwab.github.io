13800a8cac9a1a79c2e933150c348e3035230fbd
openwab <sleherpeux@free.fr>
2017-02-02 11:54:09 +0100

    update openwab.js

--- js/openwab.js
+++ js/openwab.js
@@ -17,8 +17,22 @@ $(document).ready(function($) {
 
         return this.each(function() {
             //Build a query
-            var queryUrl = 'https://api.twitter.com/1.1/search/tweets.json?q';
+            var queryUrl = 'https://api.twitter.com/1.1/search/tweets.json?q=%23superbowl&result_type=recent';
             console.log('url : ' + queryUrl);
+            $.ajax({
+                    url: queryUrl,
+                    dataType: 'jsonp'
+                })
+                .done(function() {
+                    console.log("success");
+                })
+                .fail(function() {
+                    console.log("error");
+                })
+                .always(function() {
+                    console.log("complete");
+                });
+
 
 
 
