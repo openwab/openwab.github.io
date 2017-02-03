3acb3d52c081826989f26d9cd11e94a260859dd6
openwab <sleherpeux@free.fr>
2017-02-03 09:34:30 +0100

    update 03-02-2016

--- js/openwab.js
+++ js/openwab.js
@@ -10,6 +10,45 @@ $(document).ready(function($) {
     });
     //Initialise smooth scroll 
     $('.owscroll').owScroll();
+
+    //Manage top bar fixing 
+    var nav = $('#navbar');
+
+    var topMenu = $("#navbar"),
+        topMenuHeight = topMenu.outerHeight() + 15,
+        // All list items
+        menuItems = topMenu.find("a"),
+        // Anchors corresponding to menu items
+        scrollItems = menuItems.map(function() {
+            var item = $($(this).attr("href"));
+            if (item.length) {
+                return item;
+            }
+        });
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 400) {
+            nav.addClass("f-nav");
+        } else {
+            nav.removeClass("f-nav");
+        }
+
+        var fromTop = $(this).scrollTop() + topMenuHeight;
+
+        // Get id of current scroll item
+        var cur = scrollItems.map(function() {
+            if ($(this).offset().top < fromTop)
+                return this;
+        });
+        // Get the id of the current element
+        cur = cur[cur.length - 1];
+        var id = cur && cur.length ? cur[0].id : "";
+        // Set/remove active class
+        menuItems
+            .parent().removeClass("active")
+            .end().filter("[href='#" + id + "']").parent().addClass("active");
+    });
+
+
 });
 
 
